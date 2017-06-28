68fcf34914324eb893d1edcd9f5bbda94d3bb7bf
Rob <robwilliams.email@gmail.com>
2017-06-28 22:08:20 +0100

    chapter 4

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
 /* Locations pages */
-router.get('/', ctrlLocations.homeList);
+router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
 router.get('/location/review/new', ctrlLocations.addReview);
 
