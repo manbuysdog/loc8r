5545cbda6ac94bd0bfc70b516e15f60801ed6531
Rob <robwilliams.email@gmail.com>
2017-07-06 18:02:29 +0100

    chapter 5

---
+++ app_server/models/db.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://localhost/Loc8r';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function (){
+    console.log('mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function (err){
+    console.log('mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function (){
+    console.log('mongoose disconnected from ' + dbURI);
+});
+
+var gracefulShutdown = function(msg, callback){
+    mongoose.connection.close(function () {
+        console.log('mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+process.once('SIGUSR2', function () {
+   gracefulShutdown('nodemon restart', function(){
+       process.kill(process.pid, 'SIGUSR2');
+   })
+});
+
+process.on('SIGINT', function () {
+   gracefulShutdown('app termination', function(){
+       process.exit(0);
+   })
+});
+
+process.on('SIGTERM', function () {
+   gracefulShutdown('heroku app shutdown', function(){
+       process.exit(0);
+   })
+});
+
+require('./locations');
