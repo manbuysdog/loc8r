ba35cf91521bb89b6106fe99915b6703b6104b04
Rob <robwilliams.email@gmail.com>
2017-07-10 18:07:38 +0100

    added mongolab db connection

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 var dbURI = 'mongodb://localhost/Loc8r';
+if (process.env.NODE_ENV === 'production') {
+    console.log(process.env.MONGODB_URI);
+    dbURI = process.env.MONGODB_URI;
+};
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', function (){
